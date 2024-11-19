8c1b776e9e4a4d45ad49c3e8f17aa88fa64a2b84
nischit <22u1391@students.git.edu>
2024-11-19 20:46:07 +0530

    navbar and sidebar layout done

--- app/client/src/components/sideBar/SideBar.tsx
+++ app/client/src/components/sideBar/SideBar.tsx
@@ -1,7 +1,37 @@
+import { CalendarClock, ChartNoAxesCombined, Home, LayoutList, UsersRound } from "lucide-react"
+import { Link } from "react-router-dom"
+
+const sideBarItems = [{
+    icon:<Home/>,
+    url:'/home'
+},
+{
+    icon:<ChartNoAxesCombined />,
+    url:'/analytics'
+},{
+    icon:<LayoutList />,
+    url:'/tasks'
+},{
+    icon:<CalendarClock/>,
+    url:'/calendar'
+},{
+    icon:<UsersRound />,
+    url:'/groups'
+},]
 
 function SideBar() {
+
+    const list = sideBarItems.map((item : any,i : any) => (
+        <Link to={item.url}>{item.icon}</Link>
+    ))
+
   return (
-    <div>SideBar</div>
+    <div className="w-[60px] border-r border-[--border-line] h-full flex flex-col items-center py-10 justify-between">
+        <div className="flex flex-col gap-5 ">
+            {list}
+        </div>
+        A
+    </div>
   )
 }
 
