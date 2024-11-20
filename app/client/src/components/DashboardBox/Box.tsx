59667bf72a269ecfebe157f1feac3ec0dc1fd196
Floki-07 <ziyadcbz007.com>
2024-11-21 03:24:35 +0530

    Created task modal

--- app/client/src/components/DashboardBox/Box.tsx
+++ app/client/src/components/DashboardBox/Box.tsx
@@ -14,18 +14,18 @@ const Box: React.FC<BoxProps> = ({ Icon, title, url }) => {
 
   return (
     <div
-      className="bg-[var(--background-2)] m-4 w-[120px] h-[99px] flex flex-col p-2 rounded-md cursor-pointer"
+      className="bg-[var(--background-2)] m-4 w-[120px] h-[109px] flex flex-col p-2 rounded-md cursor-pointer"
       onClick={() => navigate(url)} 
     >
       <div
         className="w-[32px] h-[32px] border-2 border-[var(--secondary)]
-        rounded-[2px] mt-2 mr-2 flex items-center justify-center text-[var(--secondary)]"
+        rounded-[2px] mt-2 mr-2 flex items-center justify-center text-[var(--secondary)] p-1"
       >
         {Icon }
       </div>
 
       <div className="mt-3 ">
-        <span className="text-center">{title}</span>
+        <span className="text-center w-[15vw]">{title}</span>
       </div>
     </div>
   );
